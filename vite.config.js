898c226a9fdbeb4b5693869db1b5488d2da5abd0
Salman Manoe <salmanoe@live.com>
2025-04-11 22:26:06 +0700

    Vue installation and config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            host: '127.0.0.1',
+        },
+        port: 3000,
+        host: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+            'vue': 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });

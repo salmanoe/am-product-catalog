fc922d2e3c507a071e91ae5900d783d8c5ba9b7e
Salman Manoe <salmanoe@live.com>
2025-04-17 20:46:23 +0700

    Register view product catalog

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import ProductList from '/resources/js/components/ProductList.vue';
+
+const app = createApp({});
+app.component('product-list', ProductList);
+app.mount('#app');
